aaf6c9f4678fa515dea888a2ddd74fd98eb22dc6
dan <dan13silv@gmail.com>
2019-07-18 22:01:50 -0600

    Added methods to print book sorted and reveresed.

--- CSIS 2420/A00 Books/src/books/BookApp.java
+++ CSIS 2420/A00 Books/src/books/BookApp.java
@@ -1,11 +1,27 @@
 package books;
 
+import java.util.Collections;
 import java.util.List;
 
 public class BookApp {
     public static void main(String[] args) {
         List<Book> book = Book.getList("books.csv");
-        for(Book b: book){
+        System.out.printf("Total number of books read %d%n%n", book.size());
+        System.out.println("Sorted book list:");
+        Collections.sort(book);
+        printBooks(book);
+        Collections.reverse(book);
+        System.out.println();
+        System.out.println("Reverse Order:");
+        printBooks(book);
+    }
+
+    /**
+     *
+     * @param book Book list that will be printed out
+     */
+    private static void printBooks(List<Book> book) {
+        for (Book b : book) {
             System.out.println(b);
         }
     }
